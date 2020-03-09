36675e2ef0699e7722e5a7ff60ec0f4e6bea3414
KeScript <kescript@localhost.localdomain>
2020-03-09 07:22:33 +0100

    initial commit

---
+++ App.vue
@@ -0,0 +1,30 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">My Vue Native App</text>
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      msg: 'This is a msg loaded from VueData Object'
+    }
+  }
+}
+</script>
+
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
